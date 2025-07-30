4d54885904bed40a67ec9d424bc2782f65890a73
Joshua Valencia <149948627+Nazonokage@users.noreply.github.com>
2025-07-30 21:26:25 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true, // Fail if 5173 is taken
+    open: true,      // Don't auto-open browser
+  },
 })
